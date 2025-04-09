e8e0d1ff9395a11fc1aebd126c69e80f4361599f
andrescam1 <108031745+andrescam1@users.noreply.github.com>
2025-04-08 23:47:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ def obtener_respuesta_openai(mensaje):
         return f"Lo siento, ocurrió un error al procesar tu mensaje: {e}"
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
